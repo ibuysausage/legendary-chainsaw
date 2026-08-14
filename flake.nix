ce36035565182b762a298e0aa91129df3c3280bd
ibuysausage <bannansmooth68@proton.me>
2026-08-13 21:23:16 -0400

    add flake.nix as base for nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+
+  description = "simple c++ programs";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          cmake
+          ninja
+          gcc
+        ];
+      };
+    };
+
+}
